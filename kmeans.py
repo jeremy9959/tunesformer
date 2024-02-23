7c633e1230757c2f254fefe782d91f558842d920
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2024-02-23 08:51:05 -0500

    working on kmeans

--- kmeans.py
+++ kmeans.py
@@ -4,29 +4,26 @@ import torch
 import json
 
 # %%
-index = faiss.read_index("index.faiss")
+index = faiss.read_index("faiss.index")
 res = faiss.StandardGpuResources()
 gpu_index = faiss.index_cpu_to_gpu(res, 0, index)
 a = gpu_index.reconstruct_n(0, 210000)
 
 # %%
 ncentroids = 1024
-niter = 20
+niter = 200
 verbose = True
 d = a.shape[1]
 kmeans = faiss.Kmeans(d, ncentroids, niter=niter, verbose=verbose, gpu=True)
 kmeans.train(a)
 
-# %%
-D, I = kmeans.index.search(a, 1)
-# %%
-D, I = index.search(a, 10)
-# %%
+D, I = gpu_index.search(kmeans.centroids,1)
+
 with open("data.json", "r") as f:
     data = json.load(f)
 
 
 # %%
-for x in I[1000]:
-    print(data[x]["abc notation"])
+for x in I:
+    print(data[x[0]]["abc notation"])
 # %%
