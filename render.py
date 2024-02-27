e18815281dc27e0dc182e6c3f80d0a26508c2543
Jeremy Teitelbaum <jeremy.teitelbaum@uconn.edu>
2024-02-27 08:57:11 -0500

    js rendering works

--- render.py
+++ render.py
@@ -1,15 +1,23 @@
 import sys
 
+def python_string_to_js_multiline(python_string):
+    # Replace Python newline characters with JavaScript newline characters
+    js_string = python_string.replace('\n', '\\n')
+    # Escape any backticks in the string
+    js_string = js_string.replace('`', '\\`')
+    # Wrap the string in backticks to create a JavaScript multiline string
+    js_multiline_string = '`' + js_string + '`'
+    return js_multiline_string
+
 tune = sys.argv[1]
 
-print("<script src='./abcjs-basic-min.js'></script>")
+print("<script src='js/abcjs-basic-min.js'></script>")
 print(f"<div id='paper'></div>")
 with open(tune, "r") as f:
     s = f.read()
-lines = s.split("\n")
 
-s = "\\ \n".join(lines)
+js_string = python_string_to_js_multiline(s)
 
-print('<script> var tune = "\\')
-print(s + '";')
+print(f"<script> var tune = {js_string};")
+
 print('ABCJS.renderAbc("paper", tune); </script>')
